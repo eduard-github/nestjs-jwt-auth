a1590edb61d377717899d288e8838c34683c6318
fancier21 <fancier@protonmail.com>
2022-10-17 01:30:27 +0400

    logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { Tokens } from './types';
@@ -8,21 +8,25 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/local/signup')
+  @HttpCode(HttpStatus.CREATED)
   signupLocal(@Body() dto: AuthDto): Promise<Tokens> {
     return this.authService.signupLocal(dto);
   }
 
   @Post('/local/signin')
+  @HttpCode(HttpStatus.OK)
   signinLocal(@Body() dto: AuthDto): Promise<Tokens> {
     return this.authService.signinLocal(dto);
   }
 
   @Post('/local/signup')
+  @HttpCode(HttpStatus.OK)
   logout() {
     return this.authService.logout();
   }
 
   @Post('/local/signup')
+  @HttpCode(HttpStatus.OK)
   refreshTokens() {
     return this.authService.refreshTokens();
   }
